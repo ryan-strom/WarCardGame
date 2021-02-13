ea50cfbbf0895706cb45db911359e9c95f3fa783
ryanstromberg <rstromberg916@gmail.com>
2021-02-13 18:59:57 -0500

    Add SetCards function to Deck class to initialize Card property

--- Models/Deck.cs
+++ Models/Deck.cs
@@ -8,5 +8,21 @@ namespace WarCardGame.Models
     {
         public List<Card> Cards { get; set; }
         private readonly int CardCountPerSuit = 13;
+        public Deck(){
+            this.SetCards();
+        }
+        private void SetCards(){
+            int suitCount = Enum.GetNames(typeof(Suit)).Length;
+            
+            Card[] newCards = new Card[suitCount * CardCountPerSuit];
+            for(int i=0; i<suitCount;i++){
+                for(int j = 0; j<CardCountPerSuit; j++){
+                    int ind = j + CardCountPerSuit*i;
+
+                    newCards[ind] =  new Card((Suit)i, j+1);
+                }
+            }
+            Cards = newCards.ToList();
+        }
     }
 }
