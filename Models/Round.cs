576d744e82948b11d3514fddfdf7301fa545c5c4
ryanstromberg <rstromberg916@gmail.com>
2021-02-13 18:28:40 -0500

    Remove abstract from Round class

--- Models/Round.cs
+++ Models/Round.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 
 namespace WarCardGame.Models
 {
-    public abstract class Round
+    public class Round
     {
         public Card PlayerCard { get; set; }
         public Card ComputerCard { get; set; }
