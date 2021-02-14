d74d389f17be5c2ffafbe3b67db10aedc1647b6c
ryanstromberg <rstromberg916@gmail.com>
2021-02-13 19:55:55 -0500

    Move DrawCard function from PlayedDeck to PlayingDeck

--- Models/PlayedDeck.cs
+++ Models/PlayedDeck.cs
@@ -2,13 +2,5 @@ namespace WarCardGame.Models
 {
     public class PlayedDeck : Deck
     {
-        //  <summary>
-        //  Pops Card Stack to return drawn Card
-        //  </summary>
-        //  <param>None</param>
-        //  <returns>An instance of the <see cref="Card"/> class representing the card that was drawn</returns>
-        public Card DrawCard(){ 
-            return Cards.Pop();
-        }
     }
 }
